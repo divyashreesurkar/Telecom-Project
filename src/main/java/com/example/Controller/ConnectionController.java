df86ae3cdf7e7410fa2b02a0d7e8411e5fece048
divyashreesurkar <divyashree.surkar97@gmail.com>
2023-06-27 12:56:57 +0530

    Added code

--- src/main/java/com/example/Controller/ConnectionController.java
+++ src/main/java/com/example/Controller/ConnectionController.java
@@ -1,18 +1,19 @@
 package com.example.Controller;
 
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Entity.Connection;
 import com.example.Service.ConnectionService;
+
 
 
 
@@ -35,7 +36,10 @@ public class ConnectionController {
 		return ResponseEntity.ok(connection);
 	}
 	
-	
+	@PutMapping("/update/{connection_id}")
+	public Connection updateConnection(@PathVariable("connection_id") String connection_id,@RequestBody Connection con) {
+		return conService.updateConnection(connection_id, con);
+	}
 	
 	
 }
--- src/main/java/com/example/Service/ConnectionService.java
+++ src/main/java/com/example/Service/ConnectionService.java
@@ -11,7 +11,6 @@ import com.example.Repository.ConnectionRepository;
 
 
 
-
 @Service
 public class ConnectionService {
 
@@ -29,6 +28,30 @@ public class ConnectionService {
     			return con.get();
     		}
     		return null;
+    	}
+        
+        
+        public Connection updateConnection(String connection_id, Connection newEntity) {
+    		Connection availableEntity = conRepository.findById(connection_id).get();// 1 chetana 10000
+
+    		if (availableEntity != null) {
+    			if (newEntity.getConnection_name() != null) {
+    				availableEntity.setConnection_name(newEntity.getConnection_name()); // chetana ---> Raj
+    			}
+    			if (newEntity.getConnection_plan() != null) {
+    				availableEntity.setConnection_plan(newEntity.getConnection_plan()); // 10000 ---->15000
+    			}
+    			if (newEntity.getConnection_status() != null) {
+    				availableEntity.setConnection_status(newEntity.getConnection_status()); // 10000 ---->15000
+    			}
+    			if (newEntity.getConnection_reference_no() > 0) {
+    				availableEntity.setConnection_reference_no(newEntity.getConnection_reference_no());; // 10000 ---->15000
+    			}
+    			return conRepository.save(availableEntity);
+    		} else {
+    			return null;
+    		}
+
     	}
 	
 }
